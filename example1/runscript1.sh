baf5e6eaedf4bd58d7d70ed14550b9fa6e95fdb2
Martin Lindén <bmelinden@gmail.com>
2013-09-06 00:09:29 +0200

    cleaned up and commented runscript1.sh in example1/

--- example1/runscript1.sh
+++ example1/runscript1.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-for RI in runinput1.m
+
+# This is a bash-script used to run analysis on Ubuntu linux
+# systems. VB7_batch_run.m keeps tracks of which trajectories are
+# done, so simple parallellization is achieved by running several
+# scripts simultaneously (one for each CPU). On other systems, you
+# will have to create your own script.
+
+for RI in runinput1.m # loop over multiple runinput files (ony one here).
 do
-
-    for i in {1..5}
+    for i in {1..5}   # number of trajectories to analyze at each call
+		      # to this script
     do
-
 	echo "########## file" $RI", round" $i 
+	# main call to matlab to perform one round of analysis
 	echo "VB7_batch_run('${RI}'),exit" | nice -n 15 matlab -nosplash -nodesktop
+	# the reason for this construction (one analysis round, then
+	# restart matlab) is our experience that matlab hoards memory
+	# when large data sets are loaded and discarded several times,
+	# and restarting matlab is the simplest way to release that
+	# memory.
     done
 done
 
