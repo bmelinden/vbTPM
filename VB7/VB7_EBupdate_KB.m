eb3fd7563429251b16a1e168c4164b2621cf721d
Martin Lindén <bmelinden@gmail.com>
2013-10-24 11:52:55 +0200

    recovered EBuptdate_KB.m after incorrect earlier merger

--- VB7/VB7_EBupdate_KB.m
+++ VB7/VB7_EBupdate_KB.m
@@ -34,7 +34,9 @@
 % with this program. If not, see <http://www.gnu.org/licenses/>.
 
 %% start of actual code
-function [n0,c0,v0,mu0]=VB7_EBupdate_KB2(n,c,v,mu,PM)
+function [n0,c0,v0,mu0]=VB7_EBupdate_KB(n,c,v,mu)
+
+
 opt1=optimset('Display','off','TolFun',1e-15,'TolX',1e-15,'MaxFunEvals',1e5,'MaxIter',1e5);
 opt2=optimset('Display','on','TolFun',1e-15,'TolX',1e-15,'MaxFunEvals',1e5,'MaxIter',1e5);
 M=size(mu,1);
@@ -52,16 +54,17 @@ v0=1/2./(sum(VS,1)/M);
 
 a0=mean(a,1);
 lnb0=mean(log(a)+lnb,1)-log(a0);
+n0=min(n,[],1);
 res=0*lnb0;
 for m=1:length(lnb0)
     % try using a logarithmic unknown to enforce exp(x)>0
-    leq=@(x)(psi(exp(x)+0.5)-log(exp(x)+0.5)-lnb0(m));  
+    leq=@(x)(psi(exp(x)+0.5)-log(exp(x)+0.5)-lnb0(m));
     
-    x0=log(PM.n(m)); % explicit initial guess
+    x0=log(0.1*n0(m));
     if(~isreal(x0))
         disp('im initial guess')
-        x0=-5;
-    end    
+        x0=-10;
+    end
     [logn,~,nflag,noutput]=fsolve(leq,x0,opt1);
     n1(m)=exp(logn);
     res1(m)=leq(logn);
@@ -69,7 +72,7 @@ for m=1:length(lnb0)
     % uning a linear unknown
     eq=@(x)(psi(x+0.5)-log(x+0.5)-lnb0(m));
     try
-        [n0(m),~,nflag,noutput]=fsolve(eq,PM.n(m),opt1);
+        [n0(m),~,nflag,noutput]=fsolve(eq,0.1*n0(m),opt1);
         res(m)=eq(n0(m));
         
         if(nflag<=0)
@@ -80,7 +83,7 @@ for m=1:length(lnb0)
         end
     catch me
         me
-    	disp('could not solve KB equations linear in n. going w log equations only.')
+            disp('could not solve KB equations linear in n. going w log equations only.')
         res(m)=inf;
         n0(m)=n1(m);
     end
@@ -90,12 +93,14 @@ for m=1:length(lnb0)
 end
 dnrel=(n1-n0)./n1;
 disp([' dn/n ' num2str(dnrel,3)])
-disp([' nres lin:  ' num2str(res,3)])
+disp([' nres lin: ' num2str(res,3)])
 disp([' nres log: ' num2str(res1,3)])
 
 %if(max(abs(dnrel))>0.1)
-%    keyboard
+% keyboard
 %end
 
 % generally use the logarithmic equation
 n0=n1;
+
+c0=(n0+0.5)./a0;
