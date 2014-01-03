c31a530ecaf9b7c446bc77a8a82bb8d5f0ec54ff
Martin Lindén <bmelinden@gmail.com>
2014-01-03 15:48:15 +0100

    Cleaned tools/ folder - moved obsolete scripts to as new tools_old/ folder - corrected paths in vbTPMstart.m

---
+++ tools_old/readme.txt
@@ -0,0 +1,5 @@
+Collection of old scripts, some of which have been used in the data
+analysis for the SJ2014 paper, but which are not needed for the source
+code release.
+
+ML 2014-01-03
--- vbTPMstart.m
+++ vbTPMstart.m
@@ -1,13 +1,12 @@
+VB7_printGPL();
 % Add paths that are needed to run the vbTPM analysis 
 
 dir0=pwd;
 addpath(genpath([dir0 filesep '.' filesep 'VB7']))
 addpath(genpath([dir0 filesep '.' filesep 'HMMcore']))
-addpath(genpath([dir0 filesep '.' filesep 'Tools']))
-%SJ added 1/1/2014:
+addpath(genpath([dir0 filesep '.' filesep 'tools']))
 addpath(genpath([dir0 filesep '.' filesep 'example1']))
 addpath([dir0 filesep '.'])
 clear dir0
 disp('Added local vbTPM paths.')
-VB7_printGPL();
 
